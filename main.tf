84a49656cf7e95137fb534a08a70d1bc3bb282ec
Arryws417 <63644214+Arryws417@users.noreply.github.com>
2020-08-20 11:44:51 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+variable    "instance_name" {}
+variable    "instance_zone" {}
+variable    "instance_type" {
+  default  = "n1-standard-1"
+}
+
+variable    "instance_subnetwork" {}
+
+resource "google_compute_instance" "vm-instance" {
+  
+  name          = "${var.instance_name}"
+  zone          = "${var.instance_zone}"
+  machine_type  = "${var.instance_type}"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+   }
+ }
+  network_interfaces {
+    subnetwork    = "${var.instance_subnetwork}"
+    access_config {
+      # allocate one-to-one NAT ip to the instance
+    }
+  }
+ }
